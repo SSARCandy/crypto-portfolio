6680d1d2f7dec07f57d57cc665bd8bd869b33c6f
ssarcandy <ssarcandy@gmail.com>
2021-04-28 14:59:48 +0000

    can rotate

--- public/app.js
+++ public/app.js
@@ -4,6 +4,11 @@ var app = new Vue({
         time: '',
         assets: [],
         nav: 0,
+        dragging: false,
+        chart_size: null,
+        chart: null,
+        startAngle: 0,
+        startAngleOffset: 0,
     },
     computed: {
         sorted: function () {
@@ -33,6 +38,35 @@ var app = new Vue({
         },
     },
     methods: {
+        getDegree(event) {
+            const x = event.touches[0].clientX;
+            const y = event.touches[0].clientY;
+            const [w, h] = this.chart_size;
+            const offset = [w / 2 - x, h / 2 - y];
+            const radians = Math.atan2(offset[1], offset[0]);
+            return (radians * 57.29) % 360;
+        },
+        startDrag(event) {
+            this.dragging = true;
+            const degree = this.getDegree(event)
+            this.startAngleOffset = this.startAngle - degree;
+        },
+        stopDrag() {
+            this.dragging = false;
+        },
+        doDrag(event) {
+            if (this.dragging) {
+                const degree = this.getDegree(event)
+
+                this.startAngle = degree + this.startAngleOffset;
+                this.chart.series[0].update({
+                    startAngle: this.startAngle
+                });
+            }
+        }
+    },
+    mounted() {
+        window.addEventListener('touchend', this.stopDrag);
     },
     created: async function () {
         const res = await fetch('/asset.json');
@@ -41,9 +75,10 @@ var app = new Vue({
         this.assets = _.sortBy(json.data, [function (o) { return -o.price * o.size; }]);
         this.nav = _.sum(this.assets.map(({ price, size }) => price * size));
 
-        $('#container').highcharts({
+        this.chart = Highcharts.chart('container', {
             chart: {
                 type: 'pie',
+                animation: false
             },
             plotOptions: {
                 pie: {
@@ -60,15 +95,18 @@ var app = new Vue({
                         overflow: 'allow',
                         crop: true
                     }
+                },
+                series: {
+                    animation: false
                 }
             },
             title: {
                 verticalAlign: 'middle',
                 floating: true,
-                text: `Total<br>$${ new Intl.NumberFormat('en-US', {
+                text: `Total<br>$${new Intl.NumberFormat('en-US', {
                     maximumFractionDigits: 0,
                     minimumFractionDigits: 0,
-                }).format(this.nav) }`,
+                }).format(this.nav)}`,
             },
             tooltip: {
                 pointFormat: '<b>{point.percentage:.1f}%</b>'
@@ -76,8 +114,9 @@ var app = new Vue({
             series: [{
                 name: '',
                 colorByPoint: true,
-                data: this.assets.map(x => [x.asset, x.price*x.size])
+                data: this.assets.map(x => [x.asset, x.price * x.size])
             }]
         });
+        this.chart_size = [$("#container").width(), $("#container").height()];
     },
 });

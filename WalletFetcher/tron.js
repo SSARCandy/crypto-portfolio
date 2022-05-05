be4fb4346ae9438b1b401c2cc118e90ae3f1f17c
ssarcandy <ssarcandy@gmail.com>
2022-05-05 06:51:42 +0000

    feat: include claimable reward (close #6)

--- WalletFetcher/tron.js
+++ WalletFetcher/tron.js
@@ -1,16 +1,14 @@
 const axios = require('axios').default;
 
 async function walletFetcher(credentials) {
-  const res = await axios.post('https://apilist.tronscan.org/api/account', {
-    address: credentials.address,
-  });
+  const res = await axios.get(`https://apilist.tronscan.org/api/account?address=${credentials.address}`);
 
   const balance_map = {};
   for (const { tokenAbbr, tokenDecimal, balance } of res.data.tokens) {
     const token = tokenAbbr.toUpperCase();
     balance_map[token] = (+balance) / (10**tokenDecimal);
   }
-  balance_map['TRX'] += res.data.voteTotal;
+  balance_map['TRX'] += res.data.voteTotal + (res.data.rewardNum / 1000000);
   const results = Object.entries(balance_map).map(([k, v]) => ({
     asset: k,
     size: v,

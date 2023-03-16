f5fa3310446be2845f15c50a24416d0ea319fed3
ssarcandy <ssarcandy@gmail.com>
2023-03-16 06:43:42 +0000

    fix: cash balance

--- WalletFetcher/firstrade.js
+++ WalletFetcher/firstrade.js
@@ -2,7 +2,7 @@ const { firstrade } = require('firstrade-cli');
 
 async function walletFetcher(credentials) {
   const positions = await firstrade.getPosition(credentials);
-  const { cashBuyingpower } = await firstrade.getBalance(credentials);
+  const { cashBalance } = await firstrade.getBalance(credentials);
   const result = positions.map(x => ({
     size: x.quantity,
     asset: x.symbol,
@@ -10,7 +10,7 @@ async function walletFetcher(credentials) {
     price: x.price, ///< special for firstrade
   }));
   result.push({
-    size: cashBuyingpower,
+    size: cashBalance,
     asset: 'USD',
     wallet: 'firstrade',
     price: 1

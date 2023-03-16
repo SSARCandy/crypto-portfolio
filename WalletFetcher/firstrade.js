354871d62be2ad401ed7f31ccea5b0603d96a396
ssarcandy <ssarcandy@gmail.com>
2023-03-16 06:55:02 +0000

    feat: cache

--- WalletFetcher/firstrade.js
+++ WalletFetcher/firstrade.js
@@ -1,6 +1,14 @@
 const { firstrade } = require('firstrade-cli');
+const fs = require('fs');
 
 async function walletFetcher(credentials) {
+  const cache_filename = `firstrade-cache-${credentials.id}.json`;
+  const now = new Date();
+  if (now.getHours() < 14 || now.getHours() > 15) {
+    const cache = JSON.parse(fs.readFileSync(cache_filename));
+    return cache;
+  }
+
   const positions = await firstrade.getPosition(credentials);
   const { cashBalance } = await firstrade.getBalance(credentials);
   const result = positions.map(x => ({
@@ -13,8 +21,9 @@ async function walletFetcher(credentials) {
     size: cashBalance,
     asset: 'USD',
     wallet: 'firstrade',
-    price: 1
+    price: 1,
   });
+  fs.writeFileSync(cache_filename, JSON.stringify(result));
 
   return result;
 }

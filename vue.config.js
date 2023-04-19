d9615547ca6c71983df38325941898b37e0520cb
SSARCandy <ssarcandy@gmail.com>
2023-04-19 07:35:55 +0000

    feat: support testbuild command

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,14 @@
-// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
-//     .BundleAnalyzerPlugin;
+const isDev = process.env.ENV === 'dev';
+const publicPath = isDev ? '' : '/crypto-portfolio';
+
 module.exports = {
+  publicPath,
   outputDir: 'docs',
-  publicPath: '/crypto-portfolio',
   productionSourceMap: false,
   devServer: {
     disableHostCheck: true,
   },
   configureWebpack: {
-    // entry: './src/main.js',
-    // plugins: [new BundleAnalyzerPlugin({
-    //     analyzerHost: '0.0.0.0'
-    // })]
   },
   pwa: {
     workboxOptions: {
@@ -20,28 +17,28 @@ module.exports = {
     manifestOptions: {
       name: 'Asset Allocation',
       short_name: 'Portfolio',
-      start_url: '/crypto-portfolio/index.html?source=pwa',
+      start_url: `${publicPath}/index.html?source=pwa`,
       theme_color: '#ffffff',
       background_color: '#ffffff',
       display: 'standalone',
       icons: [
         {
-          src: '/crypto-portfolio/icons/android-chrome-192x192.png',
+          src: `${publicPath}/icons/android-chrome-192x192.png`,
           sizes: '192x192',
           type: 'image/png',
           purpose: 'any maskable',
         },
         {
-          src: '/crypto-portfolio/icons/android-chrome-512x512.png',
+          src: `${publicPath}/icons/android-chrome-512x512.png`,
           sizes: '512x512',
           type: 'image/png',
         },
       ],
     },
     iconPaths: {
-      favicon32: '/icons/favicon-32x32.png',
-      favicon16: '/icons/favicon-16x16.png',
-      appleTouchIcon: '/icons/apple-touch-icon.png',
+      favicon32: `${publicPath}/icons/favicon-32x32.png`,
+      favicon16: `${publicPath}/icons/favicon-16x16.png`,
+      appleTouchIcon: `${publicPath}/icons/apple-touch-icon.png`,
     },
   },
 };

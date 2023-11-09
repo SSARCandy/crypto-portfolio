302c19d11e485ee004a9ace49e4dcf87d4d10706
SSARCandy <ssarcandy@gmail.com>
2023-11-09 18:58:57 +0000

    fix: migrate to tzpro.io

--- WalletFetcher/tezos.js
+++ WalletFetcher/tezos.js
@@ -1,7 +1,8 @@
 const axios = require('axios').default;
+const KEY = 'MIZAH9G49FW4Z63PC84TCU4PDHJ2RQR';
 
 async function walletFetcher(credentials) {
-  const res = await axios.get(`https://api.tzstats.com/explorer/account/${credentials.address}`);
+  const res = await axios.get(`https://api.tzpro.io/explorer/account/${credentials.address}?api_key=${KEY}`);
   
   return [{
     asset: 'XTZ',

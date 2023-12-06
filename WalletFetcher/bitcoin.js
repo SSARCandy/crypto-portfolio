85ce586e421d8fb12b79f9fc9b317bdbd0910e4d
SSARCandy <ssarcandy@gmail.com>
2023-12-06 08:08:17 +0000

    perf: reduce api call to avoid ratelimit

--- WalletFetcher/bitcoin.js
+++ WalletFetcher/bitcoin.js
@@ -1,19 +1,16 @@
 // https://api.blockcypher.com/v1/btc/main/addrs/bc1qhm4f3rg7geyajcy5w85c760nr7jqg8xnu5s6n8/balance
 
+const { sum } = require('lodash');
 const axios = require('axios').default;
 const URL = 'https://api.blockcypher.com/v1/btc/main/addrs';
 const decimal = 10**8;
 
 async function walletFetcher(credentials) {
-  const addresses = credentials.address.split(',');
-  let sum = 0;
-  for (const address of addresses) {
-    const res = await axios.get(`${URL}/${address}/balance`);
-    sum += (+res.data.balance);
-  }
+  const res = await axios.get(`${URL}/${credentials.address}/balance`);
+  const total = sum(res.data.map(x => +x.balance));
   return [{
     asset: 'BTC',
-    size: sum / decimal,
+    size: total / decimal,
     wallet: 'bitcoin',
   }];
 }

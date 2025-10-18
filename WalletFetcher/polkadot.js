95201b5fd79a119244ee83074a23eaae78fe9f4c
Andy Hsu <ssarcandy@gmail.com>
2025-10-18 20:57:10 +0800

    fix: dot api

--- WalletFetcher/polkadot.js
+++ WalletFetcher/polkadot.js
@@ -5,8 +5,8 @@ async function walletFetcher(credentials) {
   const wsProvider = new WsProvider('wss://rpc.polkadot.io');
   const api = await ApiPromise.create({ provider: wsProvider });
   const { data } = await api.query.system.account(credentials.address);
-
-  const balance = +(data.free.toString()) / (10 ** tokenDecimals);
+  const { free, reserved } = data;
+  const balance = ((+free) + (+reserved)) / (10 ** tokenDecimals);
   await api.disconnect();
 
   return [{
